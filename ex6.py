6094812a09a6f6d4a4ed1ecf9c84ab70d664ede0
Portabello <jasmitkakkar369@gmail.com>
2018-12-14 21:50:43 -0800

    upload

--- ex6.py
+++ ex6.py
@@ -1,19 +1,21 @@
+# declares variable
 types_of_people = 10
+# declares string with above variable
 x = f"There are {types_of_people} types of people."
 
 binary = "binary"
 do_not = "don't"
 y = f"Those who know {binary} and those who {do_not}"
-
+#prints variables
 print(x)
 print(y)
-
+#prints variables formatted within string
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
-
+#declare boolean and string with blank variable format
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
-
+#prints string formatted with variable
 print(joke_evaluation.format(hilarious))
 
 w = "This is the left side of..."
---
+++ ex9.py
@@ -0,0 +1,14 @@
+#Here's some new strange stuff, remeber type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\n"+ "\t"*3+ "Jan\n\t\t\tFeb\n\t\t\tMar\n\t\t\tApr\n\t\t\tMay\n\t\t\tJun\n\t\t\tAug"
+
+print("Here are the dats: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
